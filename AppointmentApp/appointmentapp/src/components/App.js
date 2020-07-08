8a43cfda716f3130b8efccc663bad0107bd02b82
jonriber <49167364+jonriber@users.noreply.github.com>
2020-07-08 14:06:48 -0300

    Components created

---
+++ AppointmentApp/appointmentapp/src/components/App.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+import '../css/App.css';
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+
+//we can only return a single html element, thats why we wrap it in a div element
+class App extends Component {
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
