0fac54f57b779fce01980b95c961efad7ceae091
jonriber <49167364+jonriber@users.noreply.github.com>
2020-07-09 10:54:31 -0300

    Update AddAppointments.js

--- AppointmentApp/appointmentapp/src/components/AddAppointments.js
+++ AppointmentApp/appointmentapp/src/components/AddAppointments.js
@@ -2,7 +2,9 @@ import React,{Component} from 'react';
 
 class AddAppointments extends Component {
     render() {
-        return (<div>Add Appointments</div>)
+        return (
+
+        );
     }
 };
 
